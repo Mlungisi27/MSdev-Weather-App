2cb4889aacf92d0968bab391020a0f8405ddda84
Mlungisi27 <mlungisisotiya7@gmail.com>
2025-06-13 11:33:47 +0200

    Add IDs to weather data elements in HTML and implement JavaScript for data retrieval

---
+++ script.js
@@ -0,0 +1,7 @@
+let valueSearch = document.getElementById('valueSearch');
+let city = document.getElementById('city');
+let temperature = document.getElementById('temperature');
+let description = document.getElementById('description');
+let clouds = document.getElementById('clouds');
+let humidity = document.getElementById('humidity');
+let pressure = document.getElementById('pressure');
